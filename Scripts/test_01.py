c5b6083e8f743c029200a5d1561fb10d1a082855
will <812983808@qq.com>
2019-01-25 21:45:55 +0800

    change code

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -1,10 +1,12 @@
 import allure
 class Test_Page():
 
+    @allure.step(title="这是第一次测试的测试步骤")
     def test_j_01(self):
         print("haha")
         assert True
 
+    @allure.step(title="这是第二次测试的测试步骤")
     def test_j_02(self):
         print("hello")
         assert False
